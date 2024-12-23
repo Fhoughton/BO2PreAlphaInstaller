2f7cded387fef3c3deec5e327f2e58750d00e636
Some Dude <fdphoughton@protonmail.com>
2024-12-23 06:29:49 +0000

    Write outline for installer

--- install-alpha.ps1
+++ install-alpha.ps1
@@ -1,2 +1,9 @@
-Write-Host 'Hello, World!' # Test message
+# Download The Game Files
+
+# Download Xenia Canary
+
+# Install Patch
+
+# Finish, press any key and explain map selector
+Write-Host 'Installation finished, run select-map.ps1 to load maps'
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
