5f53a0143cfca6920adfb2a6a8464c1d0276d8fb
Some Dude <fdphoughton@protonmail.com>
2024-12-23 07:32:45 +0000

    Redo readme to add instructions and change installer to reflect new docs

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Black Ops 2 Pre-Alpha Installer
-Lorem ipsum.
+## Disclaimer
+1. By using the scripts you accept responsibility for the download and execution of the Xenia emulator to run the game files, and acknowledge the legal implications of emulating the files and that its reprecussions are your responsibility
+
+## How To Install & Run The Pre-Alpha
+### Pre-Installation
+This script does not provide the pre-alpha files, you must find them yourself and place them in the same folder as the files from this project
+
+7zip must be installed for the program to work, if you do not have it then it may be downloaded from https://www.7-zip.org/
+
+### Acquiring The Installer
+To start click [here]() to get a zip file for the scripts, then extract it by right clicking in ```File Explorer``` and choosing ```Extract All``` then ```Extract```
+
+### Installing The Alpha
+With everything in place you can right click ```install-alpha.ps1``` and then choose ```Run with PowerShell```, this will open a blue window which will guide you through installing the files.
+
+### Setting The Map
+Before running the game you need to set a map externally, as the map selection is broken within the emulator. To do this right click ```select-map.ps1``` and choose ```Run with PowerShell```, which opens a blue window which lets you choose a map to make the game run.
+
+### Running The Game
+To start the game, having followed the installation, run ```start-alpha.ps1```.
--- install-alpha.ps1
+++ install-alpha.ps1
@@ -1,31 +1,23 @@
-# Download The Game Files
-Write-Host '> Before starting download merged2.rar from the leaks and place it in this folder (press any key to continue)'
+# Tell the user to read the docs 
+Write-Host '> Read README.md for pre-install instructions before starting! (press any key to continue)'
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
 # Download Xenia Canary
-## Define the url and file names
 $xenia_zip_url = "https://github.com/xenia-canary/xenia-canary/releases/download/experimental/xenia_canary.zip"
 $xenia_zip_name = "xenia_canary.zip"
-
-## Download and extract Xenia Canary
 Write-Host '> Downloading emulator'
 Invoke-WebRequest $xenia_zip_url -Out $xenia_zip_name
 Write-Host '> Extracting emulator'
 Expand-Archive $xenia_zip_name -Force
 
 # Extract the game folder
-## Get user to install 7zip
-Write-Host '> Install 7zip to continue (press any key to open website, if installed already just carry on)'
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-Start "https://www.7-zip.org/" # Open the website
-
 Write-Host '> Extracting merged2.rar via 7zip'
 $7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
 if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
     throw "7zip executable '$7zipPath' not found, did you not install it?"
 }
-
-$7zipPath x merged2.rar 
+Set-Alias Start-SevenZip $7zipPath
+Start-SevenZip x merged2.rar 
 
 # Finish, press any key and explain map selector
 Write-Host 'Installation finished, run select-map.ps1 to set the map and run-alpha.ps1 to start the game'
