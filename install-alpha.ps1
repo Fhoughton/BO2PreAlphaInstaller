d5eacead468cc1af07f9ac9a2c6ecb937acf5e77
Some Dude <fdphoughton@protonmail.com>
2024-12-23 06:53:09 +0000

    Add xenia download to installer

--- install-alpha.ps1
+++ install-alpha.ps1
@@ -1,6 +1,14 @@
 # Download The Game Files
 
 # Download Xenia Canary
+## Define the url and file names
+$xenia_zip_url = "https://github.com/xenia-canary/xenia-canary/releases/download/experimental/xenia_canary.zip"
+$xenia_zip_name = "xenia_canary.zip"
+
+## Download and extract Xenia Canary
+Invoke-WebRequest $xenia_zip_url -Out $xenia_zip_name
+Expand-Archive $xenia_zip_name -Force
+
 
 # Install Patch
 
